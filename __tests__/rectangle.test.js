e598dd1371a1a44474f6e7e0f35c4a639dfb3210
onurkaymak <onurkaymak@outlook.com>
2023-08-29 22:41:31 -0700

    Add Test #2 for getArea class method.

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -7,4 +7,9 @@ describe('Rectangle', () => {
         expect(rectangle.side1).toEqual(3);
         expect(rectangle.side2).toEqual(5);
     });
+
+    test('Test #2 - Should correctly get the area of a rectangle object', () => {
+        const rectangle = new Rectangle(3, 5);
+        expect(rectangle.getArea()).toEqual(15);
+    });
 });
