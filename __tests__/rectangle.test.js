ab068a5023a7597d4800aa72faa39226bfde97af
onurkaymak <onurkaymak@outlook.com>
2023-08-29 22:45:00 -0700

    Refactor Rectangle class tests with Jest beforeEach func

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -1,15 +1,18 @@
 import Rectangle from '../src/js/rectangle.js';
 
 describe('Rectangle', () => {
+    let rectangle;
+
+    beforeEach(() => {
+        rectangle = new Rectangle(3, 5);
+    });
 
     test('Test #1 - Should correctly create a rectangle object using two sides', () => {
-        const rectangle = new Rectangle(3, 5);
         expect(rectangle.side1).toEqual(3);
         expect(rectangle.side2).toEqual(5);
     });
 
     test('Test #2 - Should correctly get the area of a rectangle object', () => {
-        const rectangle = new Rectangle(3, 5);
         expect(rectangle.getArea()).toEqual(15);
     });
 });
